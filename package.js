8e1a2d2b18648297db5c9aeb1f1f8b3f8db72d22
John Rodney <jrodney@poeticsystems.com>
2015-12-15 14:07:45 -0600

    add api.export so that the UniUtils object is available on the client

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 
 Package.describe({
     name: 'vazco:universe-reactive-queries',
-    version: '1.0.0',
+    version: '1.0.1',
     summary: 'Reactive url query parameters (works with iron router)',
     git: 'https://github.com/vazco/meteor-universe-reactive-queries'
 });
@@ -12,5 +12,5 @@ Package.onUse(function (api) {
     api.use(['vazco:universe-utilities@1.1.6', 'iron:router@1.0.9 || =0.9.4', 'underscore', 'tracker', 'templating'], 'client');
     api.addFiles(['deparam.js'], 'client');
     api.addFiles(['query.js'], 'client');
-
+    api.export('UniUtils', 'client');
 });
